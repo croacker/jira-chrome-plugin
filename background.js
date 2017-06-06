443ec4a2ca1b697a1fa9c77ffc5131f2c2cff7f6
croacker <7Rv5699400>
2017-06-06 15:18:02 +0800

    delete commenst, format

--- background.js
+++ background.js
@@ -1,16 +1,9 @@
-// chrome.browserAction.onClicked.addListener(function (tab) {
-// 	chrome.tabs.insertCSS(tab.id, {
-//             file: "styles.css"
-//     });
-// 	chrome.tabs.executeScript(tab.ib, {
-// 		file: 'inject.js'
-// 	});
-// });
-
 chrome.tabs.onUpdated.addListener(function () {
 	chrome.tabs.insertCSS(null, {
 		file: "styles.css"
 	});
-	chrome.tabs.executeScript(null, { file: "inject.js" }, function () {
+	chrome.tabs.executeScript(null, {
+		 file: "inject.js" }, 
+		 function () {
 	});
 });
